637f007f6fd17e13106f084161bf610546863aca
Ricardopezao <ricardoaguiarpezao@gmail.com>
2025-04-22 18:02:09 +0000

    desafio aventureiro

---
+++ Desafio_ cartas.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+int main(){
+
+    //carta1
+    char Estado = 'A';
+    char Codigo [4] = "11";
+    char Cidade [20] = "Sao Paulo";
+    int Populacao = 11.452000;
+    float Area = 1521.11;
+    float PIB = 828.98;
+    int PontosTuristicos = 50;
+    double DensidadePopulacional = 8100.00;
+    float PIBPerCapita = 56724.32;
+
+    printf("Estado:%c\n",Estado);
+    printf("Codigo:%s\n",Codigo);
+    printf("Cidade:%s\n",Cidade);
+    printf("Populacao:%d\n",Populacao);~
+    printf("Area:%f\n",Area);
+    printf("PIB:%f\n",PIB);
+    printf("Pontos Turisticos:%d\n",PontosTuristicos);
+    printf("Densidade Populacional:%f\n",DensidadePopulacional);
+    printf("PIB Per Capita:%f\n",PIBPerCapita);
+    printf("Populacao / Area:%f\n",Populacao / Area);
+    printf("Populacao / PIB:%f\n",Populacao / Area);
+
+    //Carta2
+    char EstadoB = 'B';
+    char CodigoB [4] = "21";
+    char CidadeB [20] = "Rio de Janeiro";
+    int PopulacaoB = 6748000;
+    float AreaB =1200.25;
+    float PIBB =300.50;
+    int PontosTuristicosB = 30;
+    double DensidadePopulacionalB = 5622.24;
+    float PIBPerCapitaB = 44532.91;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+
+    printf("Estado:%c\n",EstadoB);
+    printf("Codigo:%s\n",CodigoB);
+    printf("Cidade:%s\n",CidadeB);
+    printf("Populacao:%d\n",PopulacaoB);
+    printf("Area:%f\n",AreaB);
+    printf("PIB:%f\n",PIBB);
+    printf("Pontos Turisticos:%d\n",PontosTuristicosB);
+    printf("Densiade Populacional:%f\n",DensidadePopulacionalB);
+    printf("PIB Per Capita:%f\n",PIBPerCapitaB);
+    printf("PopulacaoB / AreaB:%f\n",Populacao / Area);
+    printf("PopulacaoB / PIBB:%f\n",PopulacaoB / PIBB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
