a2f0f335b3d1ebef4011ef38b4b5681badb82584
Ricardopezao <ricardoaguiarpezao@gmail.com>
2025-05-19 18:13:41 +0000

    desafio intermediario usando switch

---
+++ Países.c
@@ -0,0 +1,133 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+    // carta 1 
+    // definir: país,populacao,área,pib,pontos turisticos,densidade demografica
+    char  País1 [10] = "Brasil";
+    int Populacao1 = 2130000000;
+    float Area1 = 8515767.049;
+    float Pib1 = 11.700000;
+    int Pontos_turisticos = 200;
+    float Densidade Demografica = 25.92;
+
+    //carta 2 
+    char Pais2 [15] = "Argentina";
+    int Populacao2 = 45195777;
+    float Area2 = 2780400;
+    float PIB2 = 646.08;
+    int Pontos_Turisticos2 = 100;
+    float Densidade_Demografica = 16.24;
+
+    int comparar;
+    scanf("%d", &comparar);
+
+    //comparação das cartas
+
+
+    swwitch(comparar)
+
+    case:1
+    if(Populacao1 > Populacao2) 
+    { printf("carta1 ganhou:\n");
+    } else if (carta1 < carta2)
+    {printf("carta2 ganhou:\n");
+    } else (carta1 == carta2)
+    {printf("empate:\n");
+    }
+    break;
+
+    
+    case:2
+    if(Area1 > Area2) 
+    { printf("carta1 ganhou:\n");
+    } else if (carta1 < carta2)
+    {printf("carta2 ganhou:\n");
+    } else (carta1 == carta2)
+    {printf("empate:\n");
+    }
+    break;
+
+    
+    case:3
+    if(PIB1 > PIB2) 
+    { printf("carta1 ganhou:\n");
+    } else if (carta1 < carta2)
+    {printf("carta2 ganhou:\n");
+    } else (carta1 == carta2)
+    {printf("empate:\n");
+    }
+    break;
+
+    
+    case:4
+    if(Pontos_Turisticos1 > Pontos_Turisticos2) 
+    { printf("carta1 ganhou:\n");
+    } else if (carta1 < carta2)
+    {printf("carta2 ganhou:\n");
+    } else (carta1 == carta2)
+    {printf("empate:\n");
+    }
+    break;
+
+
+    
+    case:5
+    if(Densidade_Demografica1 > Densidade_Demografica2) 
+    { printf("carta1 ganhou:\n");
+    } else if (carta1 < carta2)
+    {printf("carta2 ganhou:\n");
+    } else (carta1 == carta2)
+    {printf("empate:\n");
+    }
+    break;
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
